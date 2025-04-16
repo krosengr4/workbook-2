4829c19d704947acfd7075c998fa43fee1ff1466
krosengr4 <rosenkev4@gmail.com>
2025-04-16 10:05:26 -0500

    created project for notes and added notes on classes

---
+++ ClassNotes/src/main/java/com/pluralsight/ClassesAndMethods.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class ClassesAndMethods {
+
+    //public is an access modifier
+    public static void main(String[] args) {
+
+    }
+
+    //There are private methods
+    private void myPrivateMethod() {
+    }
+
+    //There are also protected methods. Can only be used within the package
+    protected void myProtectedMethod() {
+    }
+}
--- TheaterReservations/src/main/java/com/pluralsight/TheaterReservations.java
+++ TheaterReservations/src/main/java/com/pluralsight/TheaterReservations.java
@@ -1,10 +1,22 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class TheaterReservations {
 
     public static void main(String[] args) {
 
-        
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter your full name: ");
+        String fullName = myScanner.nextLine();
+
+        System.out.println("Enter the full date of the show you are attending (ex: 12/12/2012): ");
+        String date = myScanner.nextLine();
+
+        System.out.println("Enter the number of tickets you will need: ");
+        int numberOfTix = myScanner.nextInt();
+
 
     }
 
