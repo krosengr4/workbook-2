a96d9547dc53c9fcafad934da36a72f0de3283e5
krosengr4 <rosenkev4@gmail.com>
2025-04-17 11:56:26 -0500

    Added comments to the full name parser app

--- FullNameParser/src/main/java/com/pluralsight/FullNameParser.java
+++ FullNameParser/src/main/java/com/pluralsight/FullNameParser.java
@@ -6,20 +6,24 @@ public class FullNameParser {
 
     public static void main(String[] args) {
 
+        // Create new instance of scanner
         Scanner myScanner = new Scanner(System.in);
         System.out.println("Welcome to the Name Parser!");
 
+        // Ask user for data and store it in variable
         System.out.println("Please enter your full name: ");
         String fullName = myScanner.nextLine();
 
+        // trim the user input
         fullName = fullName.trim();
 
+        // split user data string at the " "
         String[] nameParts = fullName.split(" ");
 
+        // Print out first name
         System.out.println("First name: " + nameParts[0]);
 
-//        System.out.println(nameParts.length);
-
+        // Determine if user put in 2 or 3 names and print last name and middle name (if user included)
         if(nameParts.length == 2) {
             System.out.println("Last name: " + nameParts[1]);
         } else if (nameParts.length == 3) {
@@ -28,9 +32,6 @@ public class FullNameParser {
         } else {
             System.out.println("Error: Incorrect number of names entered. Enter only first middle and last.");
         }
-
-//        System.out.println("Middle name: " + nameParts[1]);
-//        System.out.println("Last name: " + nameParts[2]);
 
     }
 
