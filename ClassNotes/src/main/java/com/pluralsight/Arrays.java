b0c548bd08e7f58d80083889f6fbcdd083cbceb8
krosengr4 <rosenkev4@gmail.com>
2025-04-18 15:28:46 -0500

    Added notes for passing in arrays as arguments, returning arrays and copying arrays

--- ClassNotes/src/main/java/com/pluralsight/Arrays.java
+++ ClassNotes/src/main/java/com/pluralsight/Arrays.java
@@ -1,22 +1,39 @@
 package com.pluralsight;
 
 public class Arrays {
-    //! Once you set the size of the array, the size cannot change.
 
     public static void main(String[] args) {
-
+        //calling methods
         createAndPrintArray();
-        forEachLoop();
-        emptyArray();
+        System.out.println("\n");
 
-        //Passing in an array into a method as a parameter
+        forEachLoop();
+        System.out.println("\n");
+
+        emptyArray();
+        System.out.println("\n");
+
+        copyArray();
+        System.out.println("\n");
+
+        //! Passing in an array into a method as a parameter
         String[] cities = {"Austin", "San Fransisco", "New Orleans", "Los Angeles"};
         //call displayNumbers() method and pass in numbers array
         displayCities(cities);
+        System.out.println("\n");
 
-
+        //! Returning an array from a method
+        //* You don't have to specify the size of the array
+        int[] numbers = getNumbers();
+        //use for loop to print each value in numbers array
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + ", ");
+        }
+        System.out.println("\n");
     }
 
+
+    //! Creating an array and printing out its values
     static void createAndPrintArray() {
 
         int[] integerArray = {53, 22, 75, 24, 53, 02, 64};
@@ -25,23 +42,25 @@ public class Arrays {
         for (int i = 0; i < integerArray.length; i++) {
             System.out.print(integerArray[i] + " ");
         }
-        System.out.println("\n");
     }
 
-    static void forEachLoop() {
-        //Use for each loops to loop through an array
-        // for(type elementName : arrayName) { code }
 
+    //! Using a for each loop to loop through and print out values of array
+    static void forEachLoop() {
+
+        //* Syntax: for(type elementName : arrayName) { code }
         String[] names = {"Jose", "Kevin", "Brandon", "Curits", "Jamie"};
+
+        //using for each loop to print out names in array
         for(String name : names){
             System.out.print(name + ", ");
         }
-        System.out.println("\n");
     }
-    
+
+
+    //! creating an empty array and assigning values in the loop
     static void emptyArray() {
-        //? create an empty array and assign values in the loop
-        
+
         //creating array with no values will set all values to default
         int[] numbers = new int[5];
 
@@ -50,19 +69,48 @@ public class Arrays {
             numbers[i] = i * 3;
             System.out.print(" " + numbers[i]);
         }
-        System.out.println("\n");
     }
 
-    // Passing in Arrays into a method as parameters
+
+    //! Copying an array
+    static void copyArray(){
+
+        //Creating 2 arrays. 1st(original) with values, 2nd(the copy) with no values
+        String[] sportsTeams = {"Dallas Stars", "Texas Rangers", "Dallas Mavericks"}; //RIP Luka :(
+        String[] copySportsTeams = new String[3]; //<--- Specify the size of the array to match original array
+
+        //Use .arrayCopy() method
+        //* Syntax: System.arrayCopy(original array name, original starting subscript, array copy name, copy starting subscript, how many elements to move)
+        System.arraycopy(sportsTeams, 0, copySportsTeams, 0, 3);
+
+        //use loop to print out each element of array copy
+        for (int i = 0; i < copySportsTeams.length; i++) {
+            System.out.print(copySportsTeams[i] + ", ");
+        }
+    }
+
+
+    //! Passing in Arrays into a method as parameters.
+    //I call the method and pass in the parameters in the main method at the top.
     public static void displayCities(String cities[]) {
-        // Using s to format out. Puts number in front of the city when printed out.
+
+        // (Not needed) Using s to format. Puts number in front of city when printed out.
         int s = 1;
 
         for (int i = 0; i < cities.length; i++) {
             System.out.print(s + "." +  cities[i] + " ");
             s++;
         }
-        System.out.println("\n");
     }
 
+
+    //! Returning and array from a method
+    public static int[] getNumbers(){
+
+        //Creating array called numbers and giving values
+        int[] numbers = {234, 78, 345, 784};
+
+        // Returning numbers array
+        return numbers;
+    }
 }
