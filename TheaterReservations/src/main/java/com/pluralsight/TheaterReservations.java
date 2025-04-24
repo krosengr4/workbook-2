d2c53f86c2315192ae95ce40f72c2d252c0f8439
krosengr4 <rosenkev4@gmail.com>
2025-04-24 14:39:17 -0500

    Complete theater reservations exercise. WB 2 Mod 1 Exercise 4

--- TheaterReservations/src/main/java/com/pluralsight/TheaterReservations.java
+++ TheaterReservations/src/main/java/com/pluralsight/TheaterReservations.java
@@ -6,18 +6,32 @@ public class TheaterReservations {
 
     public static void main(String[] args) {
 
+        //Open scanner
         Scanner myScanner = new Scanner(System.in);
 
-        System.out.println("Enter your full name: ");
-        String fullName = myScanner.nextLine();
+        //Get users name
+        System.out.println("Enter your first name: ");
+        String firstName = myScanner.nextLine();
+        System.out.println("Enter your last name: ");
+        String lastName = myScanner.nextLine();
 
+        //Get the attendance date of the user
         System.out.println("Enter the full date of the show you are attending (ex: 12/12/2012): ");
         String date = myScanner.nextLine();
 
+        //Get the number of tickets the user will need
         System.out.println("Enter the number of tickets you will need: ");
         int numberOfTix = myScanner.nextInt();
 
+        //format user input
+        String tixFormatted;
+        if (numberOfTix == 1){
+            tixFormatted = numberOfTix + " ticket";
+        } else {
+            tixFormatted = numberOfTix + " tickets";
+        }
 
+        //Print out message with user input
+        System.out.println(tixFormatted + " reserved for " + date + " under " + lastName + ", " + firstName);
     }
-
 }
