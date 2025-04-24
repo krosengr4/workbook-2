fe38ffa7730781d60fb4ba2672a1b07fe926061a
krosengr4 <rosenkev4@gmail.com>
2025-04-24 14:24:50 -0500

    added comments

--- RollTheDice/src/main/java/Dice.java
+++ RollTheDice/src/main/java/Dice.java
@@ -4,27 +4,32 @@ public class Dice {
 
     public static void main(String[] args) {
 
-        roll();
-
+        //Create new dice
         Dice dice = new Dice();
 
+        //Variables for 1st and second roll
         int roll1;
         int roll2;
 
+        //Counters for number of times the sum was 2, 4, 6, and 7
         int twoCounter = 0;
         int fourCounter = 0;
         int sixCounter = 0;
         int sevenCounter = 0;
 
+        //For loop to roll the dice i times
         for (int i = 0; i <= 100; i++) {
 
+            //Roll each dice and add the sum
             roll1 = dice.roll();
             roll2 = dice.roll();
             int sum = roll1 + roll2;
 
+            //Print out dice results and sum of dice for each roll
             System.out.println("Roll " + i);
             System.out.println(roll1 + " - " + roll2 + " Sum: " + sum + "\n");
 
+            // if/else if statements that increase number of times the sum was 2, 4, 6, and 7
             if (sum == 2) {
                 twoCounter++;
             } else if (sum == 4) {
@@ -36,17 +41,16 @@ public class Dice {
             }
         }
 
+        //Print out number of times the sum was 2, 4, 6, and 7
         System.out.println("Number of sum of 2's: " + twoCounter);
         System.out.println("Number of sum of 4's: " + fourCounter);
         System.out.println("Number of sum of 6's: " + sixCounter);
         System.out.println("Number of sum of 7's: " + sevenCounter);
-
     }
 
-    //Method to roll the dice and return a random number
     static int roll() {
+        //get and return random number
         int randNumber = (int) (Math.random() * 6) + 1;
         return randNumber;
     }
-
 }
