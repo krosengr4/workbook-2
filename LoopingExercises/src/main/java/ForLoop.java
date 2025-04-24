f8199ff1b8465b434f0652fb1797740253e0d13f
krosengr4 <rosenkev4@gmail.com>
2025-04-24 14:18:51 -0500

    Added comments

--- LoopingExercises/src/main/java/ForLoop.java
+++ LoopingExercises/src/main/java/ForLoop.java
@@ -2,10 +2,13 @@ public class ForLoop {
 
     public static void main(String[] args) throws InterruptedException {
 
+        //for loop to count down
         for (int i = 10; i > 0; i--) {
             System.out.println(i);
+            //pause between each number
             Thread.sleep(1000);
         }
+        //Print launch after countdown
         System.out.println("Launch!");
     }
 }
--- LoopingExercises/src/main/java/WhileLoop.java
+++ LoopingExercises/src/main/java/WhileLoop.java
@@ -3,14 +3,13 @@ public class WhileLoop {
     public static void main(String[] args) {
 
         int i = 0;
-        int m = 1; //<--- m is to display the number of "I love java"'s
+        int m = 1; //<--- Counter
 
+        // Print out message i times
         while(i < 5){
             System.out.println(m + ". I love Java!");
             i++;
             m++;
         }
-
     }
-
 }
